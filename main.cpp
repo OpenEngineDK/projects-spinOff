28635194aaaf1d13c086f5eab634c6557ca236b9
asgerhoedt <asgerhoedt@gmail.com>
2010-05-11 10:35:14 +0000

    Pull pull pull again, pull until you're blue my friend

--- main.cpp
+++ main.cpp
@@ -12,35 +12,67 @@
 #include <Logging/Logger.h>
 #include <Logging/StreamLogger.h>
 #include <Core/Engine.h>
+#include <Display/Viewport.h>
+#include <Renderers/IRenderingView.h>
 
 // SimpleSetup
 #include <Utils/SimpleSetup.h>
 
-// Game factory
-//#include "GameFactory.h"
+// SDL
+#include <Display/SDLEnvironment.h>
 
-// name spaces that we will be using.
-// this combined with the above imports is almost the same as
-// fx. import OpenEngine.Logging.*; in Java.
-using namespace OpenEngine::Logging;
+// MRI
+#include <Renderers/OpenGL/MRIRenderingView.h>
+
+#include <Utils/MoveHandler.h>
+
 using namespace OpenEngine::Core;
+using namespace OpenEngine::Display;
+using namespace OpenEngine::Logging;
+using namespace OpenEngine::Renderers;
+using namespace OpenEngine::Renderers::OpenGL;
 using namespace OpenEngine::Utils;
 
 /**
- * Main method for the first quarter project of CGD.
- * Corresponds to the
- *   public static void main(String args[])
- * method in Java.
+ * Main.
  */
 int main(int argc, char** argv) {
-    // Setup logging facilities.
-    Logger::AddLogger(new StreamLogger(&std::cout));
+    IEnvironment* env = new SDLEnvironment(1024,768,32);
 
-    // Print usage info.
-    logger.info << "========= Running OpenEngine Test Project =========" << logger.end;
+    //Viewport* vp = new Viewport(env->GetFrame());
+
+    IRenderingView* rv = new MRIRenderingView();
 
     // Create simple setup
-    SimpleSetup* setup = new SimpleSetup("Example Project Title");
+    SimpleSetup* setup = new SimpleSetup("SpinOff", vp, env, rv);
+
+    // Create Scene
+    ISceneNode* mrinode = new MRINode();
+    setup->SetScene(*mrinode);
+
+    // Add cam
+    /*
+    ToolChain* chain = new ToolChain();
+    CameraTool* ct = new CameraTool(false);
+    chain->PushBackTool(ct);    
+
+    MouseSelection* ms = new MouseSelection(env->GetFrame(), setup->GetMouse(), NULL);
+    ms->BindTool(vp, chain);
+    
+    setup->GetKeyboard().KeyEvent().Attach(*ms);
+    setup->GetMouse().MouseMovedEvent().Attach(*ms);
+    setup->GetMouse().MouseButtonEvent().Attach(*ms);
+    setup->GetRenderer().PostProcessEvent().Attach(*ms);
+    */
+    
+    // Register the handler as a listener on up and down keyboard events.
+    MoveHandler* move_h = new MoveHandler(*(setup->GetCamera()), setup->GetMouse());
+    setup->GetKeyboard()->KeyEvent().Attach(*move_h);
+    setup->GetEngine().InitializeEvent().Attach(*move_h);
+    setup->GetEngine().ProcessEvent().Attach(*move_h);
+    setup->GetEngine().DeinitializeEvent().Attach(*move_h);
+
+
     // Start the engine.
     setup->GetEngine().Start();
 
